34f1aad84650e81a4b24f1e49b9f1d17eb34e9bf
Rahul <rahulbhatt460@gmail.com>
2023-01-29 11:42:25 +0530

    First

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,5 +1,7 @@
 package com.driver.services.impl;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
 import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
@@ -18,26 +20,81 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
+        ParkingLot parkingLot = new ParkingLot();
 
+        parkingLot.setName(name);
+        parkingLot.setAddress(address);
+
+        parkingLotRepository1.save(parkingLot);
+        return parkingLot;
     }
 
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        Spot spot = new Spot();
+        spot.setPricePerHour(pricePerHour);
+        spot.setOccupied(false);
+        if(numberOfWheels == 2){
+            spot.setSpotType(SpotType.TWO_WHEELER);
+        }
+        else if(numberOfWheels == 4){
+            spot.setSpotType(SpotType.FOUR_WHEELER);
+        }
+        else {
+            spot.setSpotType(SpotType.OTHERS);
+        }
 
+        spot.setParkingLot(parkingLot);
+
+        parkingLot.getSpotList().add(spot);
+
+
+        parkingLotRepository1.save(parkingLot);
+        return spot;
     }
 
     @Override
     public void deleteSpot(int spotId) {
+        Spot spot = spotRepository1.findById(spotId).get();
+        ParkingLot parkingLot = spot.getParkingLot();
+        List<Spot> parkingLotSpotList= parkingLot.getSpotList();
+        parkingLotSpotList.remove(spot);
+        parkingLot.setSpotList(parkingLotSpotList);
 
+        spotRepository1.delete(spot);
+        parkingLotRepository1.save(parkingLot);
     }
 
     @Override
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        Spot spot = spotRepository1.findById(spotId).get();
+        spot.setPricePerHour(pricePerHour);
 
+        List<Spot> spotList = parkingLot.getSpotList();
+        for(Spot spot1 : spotList) {
+            if(spot1 == spot) {
+                spot1.setPricePerHour(pricePerHour);
+            }
+        }
+
+
+        spotRepository1.save(spot);
+        parkingLotRepository1.save(parkingLot);
+
+        return spot;
     }
 
     @Override
     public void deleteParkingLot(int parkingLotId) {
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        List<Spot> spotList = parkingLot.getSpotList();
 
+        for(Spot spot : spotList) {
+            spotRepository1.delete(spot);
+        }
+
+        parkingLotRepository1.delete(parkingLot);
     }
 }
--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -16,6 +18,36 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+        Payment payment = new Payment();
+        int billAmount = reservation.getNumberOfHours() * reservation.getSpot().getPricePerHour();
 
+        if (amountSent < billAmount) {
+            throw new Exception("Insufficient Amount");
+        }
+
+        String paymentMode = mode.toUpperCase();
+        if(paymentMode.equals("CASH")) {
+            payment.setPaymentCompleted(true);
+            payment.setPaymentMode(PaymentMode.CASH);
+        }
+        else if (paymentMode.equals("UPI")){
+            payment.setPaymentCompleted(true);
+            payment.setPaymentMode(PaymentMode.UPI);
+        }
+        else if (paymentMode.equals("CARD")) {
+            payment.setPaymentCompleted(true);
+            payment.setPaymentMode(PaymentMode.CARD);
+        }
+        else {
+            payment.setPaymentCompleted(false);
+            throw new Exception("Payment mode not detected");
+        }
+
+        payment.setReservation(reservation);
+
+        reservationRepository2.save(reservation);
+
+        return payment;
     }
 }
